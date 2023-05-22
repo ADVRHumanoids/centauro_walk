1b6f1ffa8a76c18228da1e58c41f2cc33dd745b7
Luca Rossini <luca.rossini@iit.it>
2023-05-22 18:25:39 +0200

    first glorius commit

---
+++ python/receding_horizon.py
@@ -0,0 +1,313 @@
+import horizon.utils.utils
+from horizon.problem import Problem
+from horizon.rhc.model_description import FullModelInverseDynamics
+from horizon.rhc.taskInterface import TaskInterface
+from horizon.utils import trajectoryGenerator, resampler_trajectory
+from horizon.ros import replay_trajectory
+from horizon.utils.resampler_trajectory import Resampler
+import casadi_kin_dyn.py3casadi_kin_dyn as casadi_kin_dyn
+from matlogger2 import matlogger
+import phase_manager.pymanager as pymanager
+import phase_manager.pyphase as pyphase
+from sensor_msgs.msg import Joy
+import cartesian_interface.roscpp_utils as roscpp
+
+import casadi as cs
+import rospy
+import rospkg
+import numpy as np
+import subprocess
+import os
+import time
+
+global joy_msg
+import utils
+
+def joy_callback(msg):
+    global joy_msg
+    joy_msg = msg
+
+
+rospy.init_node('cogimon_walk_srbd')
+roscpp.init('cogimon_walk_srbd', [])
+
+# logger = matlogger.MatLogger2('/tmp/srbd_bootstrap')
+rospy.Subscriber('/joy', Joy, joy_callback)
+
+'''
+Load urdf and srdf
+'''
+
+cetc_sat_urdf_folder = rospkg.RosPack().get_path('cetc_sat_urdf')
+cetc_sat_srdf_folder = rospkg.RosPack().get_path('cetc_sat_srdf')
+
+urdf = subprocess.check_output(["xacro",
+                                cetc_sat_urdf_folder + "/urdf/cetc_sat.urdf.xacro",
+                                "sensors:=false",
+                                "upper_body:=false",
+                                "bilevel_codes:=false",
+                                "payload:=false"])
+
+srdf = subprocess.check_output(["xacro",
+                                cetc_sat_srdf_folder + "/srdf/cetc_sat.srdf.xacro",
+                                "sensors:=false",
+                                "upper_body:=false",
+                                "bilevel_codes:=false",
+                                "payload:=false"])
+urdf = urdf.decode('utf-8')
+srdf = srdf.decode('utf-8')
+
+file_dir = os.getcwd()
+
+'''
+Initialize Horizon problem
+'''
+ns = 10
+T = 1.0
+dt = T / ns
+
+prb = Problem(ns, receding=True, casadi_type=cs.SX)
+prb.setDt(dt)
+
+contact_dict = {
+    'ball_1': {
+        'type': 'point'
+    },
+
+    'ball_2': {
+        'type': 'point'
+    },
+
+    'ball_3': {
+        'type': 'point'
+    },
+
+    'ball_4': {
+        'type': 'point'
+    },
+}
+
+kin_dyn = casadi_kin_dyn.CasadiKinDyn(urdf)
+
+q_init = {'hip_roll_1': 0.0,
+          'hip_pitch_1': 0.7,
+          'knee_pitch_1': -1.4,
+          'hip_roll_2': 0.0,
+          'hip_pitch_2': 0.7,
+          'knee_pitch_2': -1.4,
+          'hip_roll_3': 0.0,
+          'hip_pitch_3': 0.7,
+          'knee_pitch_3': -1.4,
+          'hip_roll_4': 0.0,
+          'hip_pitch_4': 0.7,
+          'knee_pitch_4': -1.4}
+          # 'shoulder_yaw_1': 0.0,
+          # 'shoulder_pitch_1': 0.9,
+          # 'elbow_pitch_1': 1.68,
+          # 'wrist_pitch_1': 0.,
+          # 'wrist_yaw_1': 0.,
+          # 'shoulder_yaw_2': 0.0,
+          # 'shoulder_pitch_2': 0.9,
+          # 'elbow_pitch_2': 1.68,
+          # 'wrist_pitch_2': 0.,
+          # 'wrist_yaw_2': 0.}
+
+base_init = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
+FK = kin_dyn.fk('ball_1')
+init = base_init.tolist() + list(q_init.values())
+init_pos_foot = FK(q=init)['ee_pos']
+base_init[2] = -init_pos_foot[2]
+
+model = FullModelInverseDynamics(problem=prb,
+                                 kd=kin_dyn,
+                                 q_init=q_init,
+                                 base_init=base_init,
+                                 )
+
+rospy.set_param('mpc/robot_description', urdf)
+bashCommand = 'rosrun robot_state_publisher robot_state_publisher robot_description:=mpc/robot_description'
+process = subprocess.Popen(bashCommand.split(), start_new_session=True)
+
+ti = TaskInterface(prb=prb, model=model)
+ti.setTaskFromYaml(file_dir + '/../config/kyon_horizon_config.yaml')
+
+com_height = ti.getTask('com_height')
+com_x = ti.getTask('final_base_x')
+com_y = ti.getTask('final_base_y')
+
+com_height.setRef(np.atleast_2d(base_init).T)
+
+tg = trajectoryGenerator.TrajectoryGenerator()
+
+pm = pymanager.PhaseManager(ns)
+# phase manager handling
+c_phases = dict()
+for c in contact_dict:
+     c_phases[c] = pm.addTimeline(f'{c}_timeline')
+
+for c in contact_dict:
+    # stance phase
+    stance_duration = 10
+    stance_phase = pyphase.Phase(stance_duration, f'stance_{c}')
+    stance_phase.addItem(ti.getTask(f'{c}_contact'))
+    c_phases[c].registerPhase(stance_phase)
+
+    # short stance phase
+    # short_stance_duration = 2
+    # short_stance_phase = pyphase.Phase(short_stance_duration, f'short_stance_{c}')
+    # short_stance_phase.addItem(ti.getTask(f'foot_contact_{c}'))
+    # c_phases[c].registerPhase(short_stance_phase)
+
+    # flight phase
+    # flight_duration = 10
+    # flight_phase = pyphase.Phase(flight_duration, f'flight_{c}')
+    # init_z_foot = model.kd.fk(c)(q=model.q0)['ee_pos'].elements()[2]
+    # ref_trj = np.zeros(shape=[7, flight_duration])
+    # ref_trj[2, :] = np.atleast_2d(tg.from_derivatives(flight_duration, init_z_foot, init_z_foot, 0.05, [None, 0, None]))
+    # flight_phase.addItemReference(ti.getTask(f'foot_z_{c}'), ref_trj)
+    # c_phases[c].registerPhase(flight_phase)
+
+# pos_lf = model.kd.fk('l_sole')(q=model.q)['ee_pos']
+# pos_rf = model.kd.fk('r_sole')(q=model.q)['ee_pos']
+# base_ori = horizon.utils.utils.toRot(model.kd.fk('base_link')(q=model.q0)['ee_rot'])
+# rel_dist = base_ori.T @ (pos_lf - pos_rf)
+#
+# prb.createResidual('relative_distance_lower_x', horizon.utils.utils.barrier(rel_dist[0] + 0.3))
+# prb.createResidual('relative_distance_upper_x', horizon.utils.utils.barrier1(rel_dist[0] - 0.4))
+# prb.createResidual('relative_distance_lower_y', 10 * horizon.utils.utils.barrier(rel_dist[1] - 0.2))
+# prb.createResidual('relative_distance_upper_y', horizon.utils.utils.barrier1(rel_dist[1] + 1.))
+
+# for f_name, f in model.fmap.items():
+#     f_prev = f.getVarOffset(-1)
+#     prb.createIntermediateResidual(f'{f_name}_smooth_forces', 1e-2 * (f_prev - f), nodes=range(1, ns-1))
+
+for c in contact_dict:
+    stance = c_phases[c].getRegisteredPhase(f'stance_{c}')
+    flight = c_phases[c].getRegisteredPhase(f'flight_{c}')
+    c_phases[c].addPhase(stance)
+    c_phases[c].addPhase(stance)
+    c_phases[c].addPhase(stance)
+
+ti.model.q.setBounds(ti.model.q0, ti.model.q0, nodes=0)
+ti.model.v.setBounds(ti.model.v0, ti.model.v0, nodes=0)
+# ti.model.a.setBounds(np.zeros([model.a.shape[0], 1]), np.zeros([model.a.shape[0], 1]), nodes=0)
+ti.model.q.setInitialGuess(ti.model.q0)
+ti.model.v.setInitialGuess(ti.model.v0)
+
+f0 = [0, 0, kin_dyn.mass()/8 * 9.8]
+for cname, cforces in ti.model.cmap.items():
+    for c in cforces:
+        c.setInitialGuess(f0)
+
+# finalize taskInterface and solve bootstrap problem
+ti.finalize()
+import horizon.utils.analyzer as analyzer
+anal = analyzer.ProblemAnalyzer(prb)
+anal.print()
+ti.bootstrap()
+ti.load_initial_guess()
+solution = ti.solution
+
+# for name, element in solution.items():
+#     logger.create(name, element.shape[0])
+
+# logger.create('xig', solution['x_opt'].shape[0])
+# logger.create('xig_robot', solution['x_opt'].shape[0])
+# logger.create('q_res', solution['q'].shape[0])
+
+iteration = 0
+rate = rospy.Rate(1 / dt)
+
+contact_list_repl = list(contact_dict.keys())
+repl = replay_trajectory.replay_trajectory(dt, model.kd.joint_names(), np.array([]), {k: None for k in model.fmap.keys()},
+                                           model.kd_frame, model.kd, trajectory_markers=contact_list_repl) #, future_trajectory_markers={'srbd/base_link': 'world', 'srbd/l_sole': 'world', 'srbd/r_sole': 'world'})
+
+base_weight = 0.1
+global joy_msg
+
+xig = np.empty([prb.getState().getVars().shape[0], 1])
+
+time_elapsed_shifting_list = list()
+
+solve_flag = True
+while not rospy.is_shutdown():
+    # set initial state and initial guess
+    shift_num = -1
+
+    x_opt = solution['x_opt']
+    xig = np.roll(x_opt, shift_num, axis=1)
+    for i in range(abs(shift_num)):
+        xig[:, -1 - i] = x_opt[:, -1]
+
+    prb.getState().setInitialGuess(xig)
+    prb.setInitialState(x0=xig[:, 0])
+
+    # u_opt = solution['u_opt']
+    # uig = np.roll(u_opt, shift_num, axis=1)
+    # for i in range(abs(shift_num)):
+    #     uig[:, -1 - i] = u_opt[:, -1]
+    #
+    # prb.getInput().setInitialGuess(uig)
+
+    # shift phases of phase manager
+    tic = time.time()
+    pm._shift_phases()
+    time_elapsed_shifting = time.time() - tic
+    time_elapsed_shifting_list.append(time_elapsed_shifting)
+
+    if joy_msg.buttons[4] == 1:
+        # step
+        pass
+    else:
+        # stand
+        base_weight = 0.2
+        for c in contact_dict:
+            if c_phases[c].getEmptyNodes() > 0:
+                c_phases[c].addPhase(c_phases[c].getRegisteredPhase(f'stance_{c}'))
+
+    if np.abs(joy_msg.axes[1]) > 0.1:
+        # move com on x axis
+        final_base_x = ti.getTask('final_base_x')
+        reference = np.atleast_2d(np.array([solution['q'][0, 0] + base_weight * joy_msg.axes[1], 0., 0., 0., 0., 0., 0.]))
+        final_base_x.setRef(reference.T)
+        pass
+    else:
+        # move it back in the middle
+        final_base_x = ti.getTask('final_base_x')
+        reference = np.atleast_2d(np.array([solution['q'][0, 0], 0., 0., 0., 0., 0., 0.]))
+        final_base_x.setRef(reference.T)
+        pass
+
+    if np.abs(joy_msg.axes[0]) > 0.1:
+        # move com on y axis
+        final_base_y = ti.getTask('final_base_y')
+        reference = np.atleast_2d(np.array([0., solution['q'][0, 0] + base_weight * joy_msg.axes[1], 0., 0., 0., 0., 0.]))
+        final_base_y.setRef(reference.T)
+        pass
+    else:
+        # move com back
+        final_base_y = ti.getTask('final_base_y')
+        reference = np.atleast_2d(np.array([0., solution['q'][1, 0], 0., 0., 0., 0., 0.]))
+        final_base_y.setRef(reference.T)
+        pass
+
+    if np.abs(joy_msg.axes[5]) > 0.1:
+        # change com height
+        pass
+
+    iteration = iteration + 1
+
+    # solve real time iteration
+    ti.rti()
+    solution = ti.solution
+
+    # replay stuff
+    repl.frame_force_mapping = {cname: solution[f.getName()] for cname, f in ti.model.fmap.items()}
+    repl.publish_joints(solution['q'][:, 0])#, prefix='srbd')
+    repl.publishContactForces(rospy.Time.now(), solution['q'][:, 0], 0)
+    # repl.publish_future_trajectory_marker('srbd/base_link', solution['q'][0:3, :])
+    rate.sleep()
+
+
+print(f'average time elapsed shifting: {sum(time_elapsed_shifting_list) / len(time_elapsed_shifting_list)}')
+
