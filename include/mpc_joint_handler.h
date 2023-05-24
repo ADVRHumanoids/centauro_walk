905c37b210367e32da79efdc83eecb9b302753d6
Luca Rossini <luca.rossini@iit.it>
2023-05-24 16:47:53 +0200

    reference tranformed in maps

--- include/mpc_joint_handler.h
+++ include/mpc_joint_handler.h
@@ -9,6 +9,8 @@
 #include <XBotInterface/RobotInterface.h>
 #include <cartesian_interface/utils/RobotStatePublisher.h>
 
+#include <algorithm>
+
 class MPCJointHandler : public MPCHandler {
 public:
     MPCJointHandler(ros::NodeHandle nh,
@@ -22,10 +24,28 @@ private:
 
     void mpc_joint_callback(const trajectory_msgs::JointTrajectoryConstPtr msg);
 
-    Eigen::VectorXd _q;
-    Eigen::VectorXd _qdot;
-    Eigen::VectorXd _qddot;
-    Eigen::VectorXd _tau;
+    template<typename key, typename value>
+    void vectors_to_map(const std::vector<key> vec1, const Eigen::Matrix<value, 1, -1> vec2, std::unordered_map<key, value>& map)
+    {
+//        std::vector<value> stl_vec2(vec2.data(), vec2.data() + vec2.size());
+//        std::transform(vec1.begin(), vec1.end(), stl_vec2.begin(), std::inserter(map, map.end()), [](key a, value b)
+//        {
+//            return std::make_pair(a, b);
+//        });
+        if (vec1.size() != vec2.size())
+            throw std::runtime_error("you are trying to merge two vectors of different size in the same map!");
+
+        for (int i = 0; i < vec1.size(); i++)
+        {
+            map[vec1[i]] = vec2[i];
+        }
+    }
+
+    XBot::JointNameMap _q;
+    XBot::JointNameMap _qdot;
+    XBot::JointNameMap _qddot;
+    XBot::JointNameMap _tau;
+    std::vector<std::string> _joint_names;
 
     trajectory_msgs::JointTrajectory _mpc_solution;
 
--- src/mpc_joint_handler.cpp
+++ src/mpc_joint_handler.cpp
@@ -10,18 +10,22 @@ _robot(robot)
 {
     init_publishers_and_subscribers();
 
-    _q.resize(_model->getJointNum());
-    _qdot.resize(_model->getJointNum());
-    _qddot.resize(_model->getJointNum());
-    _tau.resize(_model->getJointNum());
+    _model->getJointPosition(_q);
+    _model->getJointVelocity(_qdot);
+    _model->getJointAcceleration(_qddot);
+    _model->getJointEffort(_tau);
 }
 
 void MPCJointHandler::mpc_joint_callback(const trajectory_msgs::JointTrajectoryConstPtr msg)
 {
     _mpc_solution = *msg;
+    if (!_is_callback_done)
+    {
+        _joint_names.insert(_joint_names.begin(), std::begin(_mpc_solution.joint_names), std::end(_mpc_solution.joint_names));
+        _joint_names.insert(_joint_names.begin(), {"VIRTUALJOINT_1", "VIRTUALJOINT_2", "VIRTUALJOINT_3", "VIRTUALJOINT_4", "VIRTUALJOINT_5", "VIRTUALJOINT_6"});
+    }
     _is_callback_done = true;
     _solution_index = 1;
-    std::cout << "francescoruscelli" << std::endl;
 }
 
 void MPCJointHandler::init_publishers_and_subscribers()
@@ -31,28 +35,29 @@ void MPCJointHandler::init_publishers_and_subscribers()
 
 bool MPCJointHandler::update()
 {
-    std::cout << "camadonna" << std::endl;
     // Read the mpc solution
     trajectory_msgs::JointTrajectoryPoint trj_point;
     trj_point = _mpc_solution.points[_solution_index];
+    std::vector<std::string> joint_names(_mpc_solution.joint_names.data(), _mpc_solution.joint_names.data() + _mpc_solution.joint_names.size());
 
     // From quaternion to RPY
-    Eigen::Quaterniond q(trj_point.positions[6], trj_point.positions[3], trj_point.positions[4], trj_point.positions[5]);
-    auto rpy = q.toRotationMatrix().eulerAngles(0, 1, 2);
+    Eigen::Quaterniond quat(trj_point.positions[6], trj_point.positions[3], trj_point.positions[4], trj_point.positions[5]);
+    auto rpy = quat.toRotationMatrix().eulerAngles(0, 1, 2);
+    Eigen::VectorXd q_euler;
+    q_euler.head(6) << trj_point.positions[0], trj_point.positions[1], trj_point.positions[2], rpy[0], rpy[1], rpy[2];
+    q_euler.tail(_model->getJointNum() - 6) = Eigen::VectorXd::Map(trj_point.positions.data() + 7, trj_point.positions.size() - 7);
 
-    _q.head(3) << trj_point.positions[0], trj_point.positions[1], trj_point.positions[2];
-    _q.block<3, 1>(3, 1) = rpy;
-    _q.tail(_model->getJointNum() - 6) = Eigen::VectorXd::Map(trj_point.positions.data() + 6, trj_point.positions.size() - 6);
-    _qdot = Eigen::VectorXd::Map(trj_point.velocities.data(), trj_point.velocities.size());
-    _qddot = Eigen::VectorXd::Map(trj_point.accelerations.data(), trj_point.accelerations.size());
-    _tau = Eigen::VectorXd::Map(trj_point.effort.data(), trj_point.effort.size());
+    vectors_to_map<std::string, double>(_joint_names, q_euler, _q);
+    vectors_to_map<std::string, double>(_joint_names, Eigen::VectorXd::Map(trj_point.velocities.data(), trj_point.velocities.size()), _qdot);
+    vectors_to_map<std::string, double>(_joint_names, Eigen::VectorXd::Map(trj_point.accelerations.data(), trj_point.accelerations.size()), _qddot);
+    vectors_to_map<std::string, double>(_joint_names, Eigen::VectorXd::Map(trj_point.effort.data(), trj_point.effort.size()), _qdot);
 
     if (_robot)
     {
         _robot->sense();
-        _robot->setPositionReference(_q.tail(_model->getJointNum() - 6));
-        _robot->setVelocityReference(_qdot.tail(_model->getJointNum() - 6));
-        _robot->setEffortReference(_tau.tail(_model->getJointNum() - 6));
+        _robot->setPositionReference(_q);
+        _robot->setVelocityReference(_qdot);
+        _robot->setEffortReference(_tau);
         _robot->move();
     }
     else
