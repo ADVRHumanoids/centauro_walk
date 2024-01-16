864f8344eae00964c319f4fb7200778f09e70c9e
FrancescoRuscelli <francescoruscelli24@gmail.com>
2024-01-16 19:34:42 +0100

    [not working] adding ee to concert

--- python/concert_receding_horizon.py
+++ python/concert_receding_horizon.py
@@ -29,12 +29,76 @@ import numpy as np
 import subprocess
 import os
 import time
-
+import yaml
 import horizon.utils as utils
+from cartesian_interface.pyci_all import *
 
 global base_pose
 global base_twist
 
+class CartesioSolver:
+
+    def __init__(self, urdf, srdf, pb, ti, ee_name, mpc_task) -> None:
+
+        self.base_fk = ti.model.kd.fk('base_link')
+        self.xbot_model = xbot.ModelInterface(get_xbot_config(urdf=urdf, srdf=srdf))
+
+        self.ee_name = ee_name
+        self.mpc_task = mpc_task
+
+        self.ti = ti
+
+        # self.ee_pos = ee_pos
+
+        self.set_model_from_solution(self.ti.solver_bs)
+
+        self.ci = pyci.CartesianInterface.MakeInstance(
+            solver='',
+            problem=pb,
+            model=self.xbot_model,
+            dt=0.01)
+
+        self.ciros = pyci.RosServerClass(self.ci)
+
+        self.task = self.ci.getTask(self.ee_name)
+
+    def set_model_from_solution(self, solver):
+
+        q = solver.getSolutionDict()['q'][:, 1]
+        # dq = self.solver_rti.getSolutionDict()['v'][:, 1]
+        base_pos = self.base_fk(q=q)['ee_pos'].toarray()
+        base_rot = self.base_fk(q=q)['ee_rot'].toarray()
+
+        qmdl = np.zeros(self.xbot_model.getJointNum())
+
+        qmdl[6:] = q[7:]
+        base_pose = Affine3(pos=base_pos)
+        base_pose.linear = base_rot
+        self.xbot_model.setJointPosition(qmdl)
+        self.xbot_model.setFloatingBasePose(base_pose)
+        self.xbot_model.update()
+
+    def update_ci(self):
+
+        self.ciros.run()
+        Tref = self.task.getPoseReference()[0]
+        xy_ref = np.array([Tref.translation.tolist() + [0, 0, 0, 1]]).T
+        # self.mpc_task.setRef(xy_ref)
+        # final_base_xyz.setRef(Tref.translation.tolist() + [0, 0, 0, 1])
+
+def ci_interface(urdf, srdf, ti, ee_name, mpc_task):
+
+    pb_dict = {
+        'stack': [['ee']],
+
+        'ee': {'type': 'Cartesian',
+               'distal_link': ee_name}
+    }
+
+    pb = yaml.dump(pb_dict)
+    ci = CartesioSolver(urdf, srdf, pb, ti, ee_name, mpc_task)
+
+    return ci
 
 def gt_pose_callback(msg):
     global base_pose
@@ -121,6 +185,8 @@ Load urdf and srdf
 with open("modularbot.urdf", "rb") as f:
     urdf = f.read().decode("UTF-8")
 
+with open("modularbot.srdf", "rb") as f:
+    srdf = f.read().decode("UTF-8")
 # get from ros param the urdf and srdf
 
 # urdf = rospy.get_param(param_name='/robot_description', default='')
@@ -208,6 +274,10 @@ ti = TaskInterface(prb=prb, model=model)
 
 # ti.setTaskFromYaml(rospkg.RosPack().get_path('kyon_controller') + '/config/concert_config.yaml')
 ti.setTaskFromYaml('/home/francesco/catkin_ws/external/kyon_controller/config/concert_config.yaml')
+
+
+# ee_xyz_task = ti.getTask('ee_xyz')
+# final_base_xy = ti.getTask('final_base_xy')
 
 pm = pymanager.PhaseManager(ns)
 # phase manager handling
@@ -301,7 +371,9 @@ jc = JoyCommands(gm)
 #     zmp_pub = rospy.Publisher('zmp_pub', PointStamped, queue_size=10)
 
 # anal = analyzer.ProblemAnalyzer(prb)
-
+ee_name = 'ee_E'
+mpc_task = ti.getTask('ee_xyz')
+ci = ci_interface(urdf, srdf, ti, ee_name, mpc_task)
 # import matplotlib.pyplot as plt
 # plt.ion()  # Turn on interactive mode
 # fig, ax = plt.subplots()
@@ -336,6 +408,9 @@ while not rospy.is_shutdown():
     prb.getState().setInitialGuess(xig)
     prb.setInitialState(x0=xig[:, 0])
 
+    # update ci
+    ci.update_ci()
+
     # closed loop
     if robot is not None:
         set_state_from_robot(robot_joint_names=robot_joint_names, q_robot=q_robot, qdot_robot=qdot_robot)
@@ -353,6 +428,8 @@ while not rospy.is_shutdown():
     ti.rti()
     time_elapsed_solving = time.time() - tic
     time_elapsed_solving_list.append(time_elapsed_solving)
+
+    ci.set_model_from_solution(solver=ti.solver_rti)
 
     # line.set_ydata(ti.solver_rti.getConstraintsValues()['dynamics'][0, :])
     # #
