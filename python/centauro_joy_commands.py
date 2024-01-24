730a1da5f4c8451b0d48e0eda57e48cd7beec9fb
Luca Rossini <luca.rossini@iit.it>
2024-01-24 18:37:20 +0100

    working with base_estimation

--- python/centauro_joy_commands.py
+++ python/centauro_joy_commands.py
@@ -96,7 +96,7 @@ class GaitManager:
 class JoyCommands:
     def __init__(self, gait_manager: GaitManager):
         self.gait_manager = gait_manager
-        self.base_weight = 1.
+        self.base_weight = 0.5
         self.base_rot_weight = 0.5
         self.com_height_w = 0.1
 
