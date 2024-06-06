d14de8f65c360e293f7e445ec6b1efc70f0ec3ae
Luca Rossini <luca.rossini@iit.it>
2024-06-06 17:04:00 +0200

    steering

--- python/cogimon_receding_horizon.py
+++ python/cogimon_receding_horizon.py
@@ -84,6 +84,24 @@ def rotate_vector(vector, quaternion):
 
     return rotated_vector
 
+def incremental_rotate(q_initial: np.quaternion, d_angle, axis) -> np.quaternion:
+    # np.quaternion is [w,x,y,z]
+    q_incremental = np.array([np.cos(d_angle / 2),
+                              axis[0] * np.sin(d_angle / 2),
+                              axis[1] * np.sin(d_angle / 2),
+                              axis[2] * np.sin(d_angle / 2)
+                              ])
+
+    # normalize the quaternion
+    q_incremental /= np.linalg.norm(q_incremental)
+
+    # initial orientation of the base
+
+    # final orientation of the base
+    q_result = np.quaternion(*q_incremental) * np.quaternion(*q_initial)
+
+    return q_result
+
 
 rospy.init_node('cogimon_walk_srbd')
 
@@ -115,53 +133,58 @@ cfg.set_bool_parameter('is_model_floating_base', True)
 
 base_pose = None
 base_twist = None
-# try:
-#     robot = xbot.RobotInterface(cfg)
-#     # rospy.Subscriber('/cogimon_base_estimation/base_link/pose', PoseStamped, gt_pose_callback)
-#     # rospy.Subscriber('/cogimon_base_estimation/base_link/twist', TwistStamped, gt_twist_callback)
-#     rospy.Subscriber('/xbotcore/link_state/base_link/pose', PoseStamped, gt_pose_callback)
-#     rospy.Subscriber('/xbotcore/link_state/base_link/twist', TwistStamped, gt_twist_callback)
-#
-#     while base_pose is None or base_twist is None:
-#         rospy.sleep(0.01)
-#
-#     robot.sense()
-#     q_init = robot.getPositionReference()
-#     q_init = robot.eigenToMap(q_init)
+try:
+    robot = xbot.RobotInterface(cfg)
+    # rospy.Subscriber('/cogimon_base_estimation/base_link/pose', PoseStamped, gt_pose_callback)
+    # rospy.Subscriber('/cogimon_base_estimation/base_link/twist', TwistStamped, gt_twist_callback)
+    rospy.Subscriber('/xbotcore/link_state/base_link/pose', PoseStamped, gt_pose_callback)
+    rospy.Subscriber('/xbotcore/link_state/base_link/twist', TwistStamped, gt_twist_callback)
 
-# except:
-print(colorama.Fore.RED + 'RobotInterface not created' + colorama.Fore.RESET)
-base_pose = np.array([0.03, 0., 0.962, 0., -0.029995, 0.0, 0.99955])
+    while base_pose is None or base_twist is None:
+        print('trying')
+        rospy.sleep(0.01)
 
-q_init = {"WaistYaw": 0.0,
-          "WaistLat": 0.0,
-          "RShSag": 0.959931,
-          "RShLat": -0.007266,
-          "RShYaw": -0.0,
-          "RElbj": -1.919862,
-          "RForearmPlate": 0.0,
-          "RWrj1": -0.523599,
-          "LShSag": 0.959931,
-          "LShLat": 0.007266,
-          "LShYaw": -0.0,
-          "LElbj": -1.919862,
-          "LForearmPlate": 0.0,
-          "LWrj1": -0.523599,
-          "RHipLat": 0.0,
-          "RHipSag": -0.363826,
-          "RHipYaw": 0.0,
-          "RKneePitch": 0.731245,
-          "RAnklePitch": -0.307420,
-          "RAnkleRoll": -0.0,
-          "LHipLat": -0.0,
-          "LHipSag": -0.363826,
-          "LHipYaw": 0.0,
-          "LKneePitch": 0.731245,
-          "LAnklePitch": -0.307420,
-          "LAnkleRoll": 0.0}
+    base_pose = np.array([0.03, 0., 0.962, 0., -0.029995, 0.0, 0.99955])
+    base_twist = np.zeros(6)
 
-base_twist = np.zeros(6)
-robot = None
+    robot.sense()
+    q_init = robot.getPositionReference()
+    q_init = robot.eigenToMap(q_init)
+    print(colorama.Fore.CYAN + 'RobotInterface created!' + colorama.Fore.RESET)
+
+except:
+    print(colorama.Fore.CYAN + 'RobotInterface not created' + colorama.Fore.RESET)
+    base_pose = np.array([0.03, 0., 0.962, 0., -0.029995, 0.0, 0.99955])
+
+    q_init = {"WaistLat": 0.0,
+              # "WaistYaw": 0.0,
+              "RShSag": 0.959931,
+              "RShLat": -0.007266,
+              "RShYaw": -0.0,
+              "RElbj": -1.919862,
+              "RForearmPlate": 0.0,
+              "RWrj1": -0.523599,
+              "LShSag": 0.959931,
+              "LShLat": 0.007266,
+              "LShYaw": -0.0,
+              "LElbj": -1.919862,
+              "LForearmPlate": 0.0,
+              "LWrj1": -0.523599,
+              "RHipLat": 0.0,
+              "RHipSag": -0.363826,
+              "RHipYaw": 0.0,
+              "RKneePitch": 0.731245,
+              "RAnklePitch": -0.307420,
+              "RAnkleRoll": -0.0,
+              "LHipLat": -0.0,
+              "LHipSag": -0.363826,
+              "LHipYaw": 0.0,
+              "LKneePitch": 0.731245,
+              "LAnklePitch": -0.307420,
+              "LAnkleRoll": 0.0}
+
+    base_twist = np.zeros(6)
+    robot = None
 
 '''
 Initialize Horizon problem
@@ -177,12 +200,14 @@ dt_param = prb.createParameter('dt', 1)
 dt_param.assign(dt)
 prb.setDt(dt_param)
 
-kin_dyn = casadi_kin_dyn.CasadiKinDyn(urdf)
+fixed_joint_map = {'WaistYaw': 0.0}
+kin_dyn = casadi_kin_dyn.CasadiKinDyn(urdf, fixed_joints=fixed_joint_map)
 
 model = FullModelInverseDynamics(problem=prb,
                                  kd=kin_dyn,
                                  q_init=q_init,
-                                 base_init=base_pose)
+                                 base_init=base_pose,
+                                 fixed_joint_map=fixed_joint_map)
 
 rospy.set_param('/robot_description', urdf)
 bashCommand = 'rosrun robot_state_publisher robot_state_publisher'
@@ -203,13 +228,13 @@ Foot vertices relative distance constraint
 '''
 pos_lf = model.kd.fk('l_sole')(q=model.q)['ee_pos']
 pos_rf = model.kd.fk('r_sole')(q=model.q)['ee_pos']
-base_ori = utils.toRot(model.kd.fk('base_link')(q=model.q0)['ee_rot'])
-rel_dist = base_ori @ (pos_lf - pos_rf)
+base_ori = model.kd.fk('base_link')(q=model.q)['ee_rot']
+rel_dist = base_ori.T @ (pos_lf - pos_rf)
 
 # prb.createResidual('relative_distance_lower_x', utils.barrier(rel_dist[0] + 0.3))
 # prb.createResidual('relative_distance_upper_x', utils.barrier1(rel_dist[0] - 0.4))
-prb.createResidual('relative_distance_lower_y', 10. * utils.barrier(rel_dist[1] - 0.25))
-prb.createResidual('relative_distance_upper_y', utils.barrier1(rel_dist[1] + 1.))
+prb.createResidual('relative_distance_lower_y', 10. * utils.barrier(rel_dist[1] - 0.21))
+# prb.createResidual('relative_distance_upper_y', 10. * utils.barrier1(rel_dist[1] - 0.35))
 
 force_z_ref = dict()
 for contact, force in model.getForceMap().items():
@@ -283,7 +308,8 @@ rate = rospy.Rate(1 / dt)
 contact_list_repl = list(model.cmap.keys())
 repl = replay_trajectory.replay_trajectory(dt, model.kd.joint_names(), np.array([]),
                                            {k: None for k in model.fmap.keys()},
-                                           model.kd_frame, model.kd, trajectory_markers=contact_list_repl)
+                                           model.kd_frame, model.kd, trajectory_markers=contact_list_repl,
+                                           fixed_joint_map=fixed_joint_map)
 
 
 def step(swing, stance):
@@ -310,16 +336,18 @@ while not rospy.is_shutdown():
     prb.getState().setInitialGuess(xig)
     prb.setInitialState(x0=xig[:, 0])
 
-    if False: #robot is not None:
+    if robot is not None:
         robot.sense()
-        q = robot.getJointPosition()
-        # q = robot.getPositionReference()
-        q = np.hstack([base_pose, q])
-        model.q.setBounds(q, q, nodes=0)
-        qdot = robot.getJointVelocity()
-        # qdot = robot.getVelocityReference()
-        qdot = np.hstack([base_twist, qdot])
-        model.v.setBounds(qdot, qdot, nodes=0)
+        q = robot.getJointPositionMap()
+        for fixed_joint in fixed_joint_map.keys():
+            del q[fixed_joint]
+        # q = np.hstack([base_pose, q])
+        model.q[7:].setBounds(list(q.values()), list(q.values()), nodes=0)
+        qdot = robot.getJointVelocityMap()
+        for fixed_joint in fixed_joint_map.keys():
+            del qdot[fixed_joint]
+        # qdot = np.hstack([base_twist, qdot])
+        model.v[6:].setBounds(list(qdot.values()), list(qdot.values()), nodes=0)
 
 
     pm.shift()
@@ -338,14 +366,31 @@ while not rospy.is_shutdown():
         final_base_xy = ti.getTask('final_base_xy')
         vec = np.array([joy_msg.axes[1], joy_msg.axes[0], 0])
         rot_vec = rotate_vector(vec, solution['q'][[6, 3, 4, 5], 0])
-        reference = np.atleast_2d(np.array([solution['q'][0, 0] + 2. * rot_vec[0], solution['q'][1, 0] + 1.0 * rot_vec[1], 0., 0., 0., 0., 0.]))
+        print(rot_vec)
+        reference = np.atleast_2d(np.array([solution['q'][0, 0] + 1.5 * rot_vec[0], solution['q'][1, 0] + 1.5 * rot_vec[1], 0., 0., 0., 0., 0.]))
         final_base_xy.setRef(reference.T)
+
     else:
         final_base_xy = ti.getTask('final_base_xy')
         reference = np.atleast_2d(np.array([solution['q'][0, 0], solution['q'][1, 0], 0., 0., 0., 0., 0.]))
         final_base_xy.setRef(reference.T)
 
-    #
+    if np.abs(joy_msg.axes[3]) > 0.1:
+        base_orientation_task = ti.getTask('base_yaw_orientation')
+        base_reference_yaw = np.array([[0., 0., 0., 0, 0, 0, 0]]).T
+        d_angle = np.pi / 2 * 0.25 * joy_msg.axes[3]
+        axis = [0, 0, 1]
+        angular_velocity_vector = incremental_rotate(solution['q'][[6, 3, 4, 5], 0], d_angle, axis)
+        base_reference_yaw[3] = angular_velocity_vector.x
+        base_reference_yaw[4] = angular_velocity_vector.y
+        base_reference_yaw[5] = angular_velocity_vector.z
+        base_reference_yaw[6] = angular_velocity_vector.w
+        base_orientation_task.setRef(base_reference_yaw)
+    else:
+        base_orientation_task = ti.getTask('base_yaw_orientation')
+        base_reference_yaw = np.atleast_2d(np.array([0., 0., 0., solution['q'][3, 0], solution['q'][4, 0], solution['q'][5, 0], solution['q'][6, 0]]))
+        base_orientation_task.setRef(base_reference_yaw.T)
+
     if joy_msg.buttons[3] == 1:
         com_height = ti.getTask('com_height')
         reference = np.atleast_2d(np.array([0., 0., solution['q'][2, 0] + 0.05, 0., 0., 0., 0.]))
@@ -354,8 +399,6 @@ while not rospy.is_shutdown():
         com_height = ti.getTask('com_height')
         reference = np.atleast_2d(np.array([0., 0., solution['q'][2, 0] - 0.05, 0., 0., 0., 0.]))
         com_height.setRef(reference.T)
-
-
 
     # solve real time iteration
     ti.rti()
